9cb40e620915741700c75f305cac2d63d8f6669c
matias siano <matias.siano@outlook.com>
2024-10-29 19:46:08 -0300

    utilizando-angular-material

--- laboratorio4/src/app/app.module.ts
+++ laboratorio4/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button'; // Si usas botones de Angular Material
+import { MatIconModule } from '@angular/material/icon';
+
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SectionComponent } from './components/section/section.component';
@@ -12,6 +17,7 @@ import { AsideComponent } from './components/aside/aside.component';
 @NgModule({
   declarations: [
     AppComponent,
+
     HomePageComponent,
     HeaderComponent,
     SectionComponent,
@@ -20,10 +26,15 @@ import { AsideComponent } from './components/aside/aside.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
-    provideClientHydration(),
-    provideAnimationsAsync()
+    // provideClientHydration(),
+    // provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
--- laboratorio4/src/app/components/pages/home-page/home-page.component.ts
+++ laboratorio4/src/app/components/pages/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-home-page',
+  selector: 'mat-toolbar-row',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
