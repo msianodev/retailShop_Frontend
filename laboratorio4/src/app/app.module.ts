9799cd4abf2e440879ed3e8b35207e8fc5861522
matias siano <matias.siano@outlook.com>
2024-10-29 17:22:23 -0300

    home-page

--- laboratorio4/src/app/app.module.ts
+++ laboratorio4/src/app/app.module.ts
@@ -4,14 +4,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SectionComponent } from './components/section/section.component';
+import { AsideComponent } from './components/aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    SectionComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
     provideClientHydration(),
---
+++ laboratorio4/src/app/components/pages/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../header/header.component';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css',
+})
+export class HomePageComponent {
+
+}
