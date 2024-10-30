cfd8a0c8b77310bb56902ca24df98e587f1ff716
matias siano <matias.siano@outlook.com>
2024-10-29 21:57:28 -0300

    menu-base

--- laboratorio4/src/app/components/pages/home-page/home-page.component.ts
+++ laboratorio4/src/app/components/pages/home-page/home-page.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+
+interface MenuItem {
+  icon: string;
+  label: string;
+  route: string;
+}
 
 @Component({
   selector: 'mat-toolbar-row',
@@ -8,5 +14,37 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {
-
+  menuItems: MenuItem[] = [
+    { 
+      icon: 'store', 
+      label: 'Products', 
+      route: '/Products' 
+    },
+    { 
+      icon: 'bar_chart', 
+      label: 'Sales', 
+      route: '/sales' 
+    },
+    { 
+      icon: 'inventory_2', 
+      label: 'Inventory', 
+      route: '/inventory' 
+    },
+    { 
+      icon: 'people', 
+      label: 'Customers', 
+      route: '/customers' 
+    },
+    { 
+      icon: 'settings', 
+      label: 'Settings', 
+      route: '/settings' 
+    },
+    { 
+      icon: 'logout', 
+      label: 'Logout', 
+      route: '/logout' 
+    },
+    
+  ];
 }
